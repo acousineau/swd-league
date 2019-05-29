6e00de31250b69a3bcd501ce47c61926226b3363
Mike Kazmer <webbasser055@gmail.com>
2019-05-29 18:50:59 -0400

    signin-in-card

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
+import '../../styles/materialize.scss'
+
 const SignIn = props => {
   return (
-    <div className="">
-      <div className="">
-        <div className="">Please Sign In With Google</div>
-        <p className="">Users must log in with Google in order to use the application.</p>
-        <div className="">
-          <a className="" href="/auth/google">
-            <i className="fab fa-google" />
-            &nbsp;&nbsp;Sign In with Google
-          </a>
+    <div class="row">
+      <div class="col s12 m6">
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text">
+            <div className="card-title">Please Sign In With Google</div>
+            <p className="">Users must log in with Google in order to use the application.</p>
+            <div className="card-action">
+              <a className="" href="/auth/google">
+                <i className="fab fa-google" />
+                &nbsp;&nbsp;Sign In with Google
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
