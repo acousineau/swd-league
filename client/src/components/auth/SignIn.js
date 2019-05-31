6542bd0391cf4e9a336b73f33bbb1702453a3e07
Mike Kazmer <webbasser055@gmail.com>
2019-05-31 13:38:59 -0400

    restyled_card/centered_and_ssquare

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -1,22 +1,20 @@
 import React from 'react'
 
-import '../../styles/materialize.scss'
+import './Signin.scss'
 
 const SignIn = props => {
   return (
-    <div class="row">
-      <div class="col s12 m6">
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
-            <div className="card-title">Please Sign In With Google</div>
-            <p className="">Users must log in with Google in order to use the application.</p>
-            <div className="card-action">
-              <a className="" href="/auth/google">
-                <i className="fab fa-google" />
-                &nbsp;&nbsp;Sign In with Google
-              </a>
-            </div>
-          </div>
+    <div className="signin-container">
+      <div className="signin-card">
+        <div className="signin-card-title">Please Sign In With Google</div>
+        <p className="signin-text">
+          Users must log in with Google in order to use the application.
+        </p>
+        <div className="signin-card-action">
+          <a className="" href="/auth/google">
+            <i className="fab fa-google" />
+            &nbsp;&nbsp;Sign In with Google
+          </a>
         </div>
       </div>
     </div>
