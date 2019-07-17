940235deffc843a04065a9646e4da1a69a915a94
Mike Kazmer <webbasser055@gmail.com>
2019-07-16 20:39:02 -0400

    more cleanup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -36,30 +36,31 @@ const Header = () => {
   const userState = useContext(UserContext)[0]
   return (
     <nav className="Header">
-      <div className="header-wrapper" />
-      <div className="nav-wrapper">
-        <div className="nav-logo">
-          <i className="fas fa-jedi" />
-          <Link to="/" className="home-link" style={{ fontSize: '25px' }}>
-            SWD League
-          </Link>
-          <i className="fab fa-galactic-republic" />
-        </div>
-
-        <div className="menu-wrap">
-          <input type="checkbox" className="toggler" />
-          <div className="hamburger">
-            <div />
+      <div className="header-wrapper">
+        <div className="nav-wrapper">
+          <div className="nav-logo">
+            <i className="fas fa-jedi" />
+            <Link to="/" className="home-link" style={{ fontSize: '25px' }}>
+              SWD League
+            </Link>
+            <i className="fab fa-galactic-republic" />
           </div>
-          <div className="main-menu">
-            <ul className="nav-items">
-              <li>
-                <Link to="/leagues" className="">
-                  Leagues
-                </Link>
-              </li>
-              {renderControls(userState.user)}
-            </ul>
+
+          <div className="menu-wrap">
+            <input type="checkbox" className="toggler" />
+            <div className="hamburger">
+              <div />
+            </div>
+            <div className="main-menu">
+              <ul className="nav-items">
+                <li>
+                  <Link to="/leagues" className="">
+                    Leagues
+                  </Link>
+                </li>
+                {renderControls(userState.user)}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
